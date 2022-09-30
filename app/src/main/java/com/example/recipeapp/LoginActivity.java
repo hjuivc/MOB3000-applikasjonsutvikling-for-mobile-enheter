8f1c1bf45308923de2586a5792fd9ff27cbb2ec0
Bjørnar Borge <72221999+secretoelit@users.noreply.github.com>
2022-09-30 09:57:26 +0200

    Opprettet noen nye klasser + opprettet noen funksjoner på login.java
    La inn activity for glemt passord, registrer bruker og profil side samtidig som jeg la inn noen funksjoner på login.java som sjekker om bruker er allerede registrert og aktiviserte knapper ved hjelp av onclicklistener.

--- app/src/main/java/com/example/recipeapp/LoginActivity.java
+++ app/src/main/java/com/example/recipeapp/LoginActivity.java
@@ -1,21 +1,134 @@
 package com.example.recipeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * Lage variabler for de forskjellige elementene i layouten
+     */
+    private TextView register, forgotPassword;
+    private EditText editTextEmail, editTextPassword;
+    private Button signIn;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+
+
         /**
          Legge inn tittel på siden.
          */
         this.setTitle(getResources().getString(R.string.activity_login));
 
+        /**
+         * Aktivisere komponentene i layouten og koble de til variablene.
+         */
+        register = (TextView) findViewById(R.id.registerNewUser);
+        register.setOnClickListener(this);
 
+        forgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        forgotPassword.setOnClickListener(this);
 
+        signIn = (Button) findViewById(R.id.btnLogin);
+        signIn.setOnClickListener(this);
+
+        editTextEmail = (EditText) findViewById(R.id.txtEmail);
+        editTextEmail.setOnClickListener(this);
+
+        editTextPassword = (EditText) findViewById(R.id.txtPassword);
+        editTextPassword.setOnClickListener(this);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    /**
+     * Denne metoden blir kallet på når brukeren trykker på de ulike "knappene" i layouten.
+     */
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.registerNewUser:
+                startActivity(new Intent(this, RegisterUser.class));
+                break;
+            case R.id.forgotPassword:
+                startActivity(new Intent(this, ForgotPassword.class));
+                break;
+            case R.id.btnLogin:
+                userLogin();
+                break;
+        }
+    }
+
+    /**
+     * Denne metoden blir kallet på når brukeren trykker på "Logg inn" knappen.
+     */
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum length of password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        /**
+         * Denne metoden sjekker om brukeren er registrert i databasen.
+         */
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                if (task.isSuccessful()) {
+                    finish();
+                    Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
