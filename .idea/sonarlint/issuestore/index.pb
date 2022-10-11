03db48e26505635980d8785fafd5a08863377efd
Bjørnar Borge <72221999+secretoelit@users.noreply.github.com>
2022-10-11 09:39:27 +0200

    Legg til ingrediens knapp
    Og funksjonalitet.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,16 +1,12 @@
 
 H
 app/google-services.json,f\d\fd8d34b142d02ec743a8efefefd2c097961666e9
-9
-	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 P
  app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
 k
 ;app/src/main/java/com/example/recipeapp/ShowAllRecipes.java,2\6\2630aa074327b74aa9c51029c26af230e3a7ede0
-@
-app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
 k
 ;app/src/main/java/com/example/recipeapp/ForgotPassword.java,a\e\ae6ae75d3d9e456da9a6bdb6c9878ea09a90c4c9
 b
@@ -23,11 +19,13 @@ h
 8app/src/main/java/com/example/recipeapp/Ingredients.java,c\0\c08f2b19c409a9cb5ccd252daa8e787a19ff0fb5
 c
 3app/src/main/java/com/example/recipeapp/Recipe.java,c\d\cd1d9624062dd229437290f032b14e91abe4f3ae
-j
-:app/src/main/java/com/example/recipeapp/LoginActivity.java,9\c\9c0e243a2f74e480af5f9c847db4e267c99471b1
 l
 <app/src/main/java/com/example/recipeapp/FavoriteRecipes.java,6\5\652460e005c1a046041b21ce06549d9541fa31bf
-f
-6app/src/main/java/com/example/recipeapp/AddRecipe.java,f\5\f59e37bb3c74323c6d8bdbbd803fda90d2fda052
 i
 9app/src/main/java/com/example/recipeapp/RegisterUser.java,9\3\939bef3a0c143e78be070f53517ebedaadb070b4
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+j
+:app/src/main/java/com/example/recipeapp/LoginActivity.java,9\c\9c0e243a2f74e480af5f9c847db4e267c99471b1
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
--- app/src/main/java/com/example/recipeapp/AddRecipe.java
+++ app/src/main/java/com/example/recipeapp/AddRecipe.java
@@ -2,13 +2,18 @@ package com.example.recipeapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.Switch;
@@ -19,7 +24,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AddRecipe extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddRecipe extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     /**
      * Lage variabler for de forskjellige elementene i layouten
@@ -31,10 +39,23 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
     private Spinner spinnerCuisine, spinnerUnit;
     private Switch switchVegan;
 
+    private LinearLayout layoutList;
+    private Button buttonAdd;
+    private List<String> ingredients = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
+
+        /**
+         * Legger til elementer i array som skal inn i spinneren
+         */
+        ingredients.add("g");
+        ingredients.add("kg");
+        ingredients.add("ml");
+        ingredients.add("l");
+        ingredients.add("pcs");
 
         /**
          Legge inn tittel på siden.
@@ -57,13 +78,16 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         editRecipeName = (EditText) findViewById(R.id.txtRecipeName);
         editTextDescription = (EditText) findViewById(R.id.txtDescription);
         editStepByStep = (EditText) findViewById(R.id.txtStep);
-        editIngredient = (EditText) findViewById(R.id.txtIngredient);
-        editAmount = (EditText) findViewById(R.id.txtAmount);
         spinnerCuisine = (Spinner) findViewById(R.id.spinnerCuisine);
-        spinnerUnit = (Spinner) findViewById(R.id.spinnerUnit);
         switchVegan = (Switch) findViewById(R.id.txtVeganSwitch);
 
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        layoutList = findViewById(R.id.ingredientList);
+        buttonAdd = findViewById(R.id.btnAddIngredient);
+        spinnerUnit = findViewById(R.id.spinnerUnit);
+
+        buttonAdd.setOnClickListener(this);
 
         /**
          * Legge til elementer i cousine spinneren
@@ -72,13 +96,14 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         adapter_cousine.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCuisine.setAdapter(adapter_cousine);
 
-        /**
-         * Legge til elementer i unit spinneren
-         */
-        ArrayAdapter<CharSequence> adapter_unit = ArrayAdapter.createFromResource(this, R.array.unit_array, android.R.layout.simple_spinner_item);
-        adapter_unit.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerUnit.setAdapter(adapter_unit);
+        if (switchVegan != null) {
+            switchVegan.setOnCheckedChangeListener(this);
+        }
+
     }
+
+
+
 
     /**
      * Metode for å registrere bruker. Kalles når man trykker "save" og "add ingredients.
@@ -89,9 +114,39 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
             case R.id.btnSave:
                 saveRecipe();
                 break;
-            //TODO: Her må det legges inn case for add ingredient
+            case R.id.btnAddIngredient:
+                addIngredient();
+                break;
         }
     }
+
+    /**
+     * Metode for å legge til ingredienser
+     */
+    private void addIngredient() {
+        View cricketerView = getLayoutInflater().inflate(R.layout.row_add_ingredient, null, false);
+
+        EditText txtIngredient = cricketerView.findViewById(R.id.txtIngredient);
+        AppCompatSpinner spinnerUnit = (AppCompatSpinner) cricketerView.findViewById(R.id.spinnerUnit);
+        ImageView imageClose = (ImageView) cricketerView.findViewById(R.id.image_remove);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, ingredients);
+        spinnerUnit.setAdapter(arrayAdapter);
+
+            imageClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeView(cricketerView);
+            }
+        });
+            layoutList.addView(cricketerView);
+}
+
+    private void removeView(View view) {
+        layoutList.removeView(view);
+
+    }
+
 
     /**
      * Metode for å lagre oppskriften.
@@ -102,7 +157,6 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         String stepByStep = editStepByStep.getText().toString().trim();
         String ingredient = editIngredient.getText().toString().trim();
         String amount = editAmount.getText().toString().trim();
-        //TODO: java.lang.NullPointerException: Attempt to invoke virtual method 'java.lang.String java.lang.Object.toString()' on a null object reference
         String cuisine = spinnerCuisine.getSelectedItem().toString();
         String unit = spinnerUnit.getSelectedItem().toString();
         Boolean vegan = false;
@@ -152,11 +206,13 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
 
         FirebaseDatabase.getInstance().getReference("Recipes")
                 .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .push() // Legger til for å ikke overskrive data til databasen hver gang
                 .child(userID)
                 .setValue(recipe);
 
         FirebaseDatabase.getInstance().getReference("Ingredients")
                 .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .push() // Legger til for å ikke overskrive data til databasen hver gang
                 .child(String.valueOf(recipeID))
                 .setValue(ingredients);
 
@@ -176,5 +232,16 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            switchVegan.setText("Vegan");
+            System.out.println("Vegan");
+        } else {
+            switchVegan.setText("Not Vegan");
+            System.out.println("Not Vegan");
+        }
     }
 }
