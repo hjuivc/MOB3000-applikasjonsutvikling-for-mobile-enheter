84d22d7e55304e06bae2c39771b903fd9915d0b7
Bjørnar Borge <72221999+1d05408acb@users.noreply.github.com>
2022-10-22 19:39:29 +0200

    Update ShowAllRecipes.java

--- app/src/main/java/com/example/recipeapp/ShowAllRecipes.java
+++ app/src/main/java/com/example/recipeapp/ShowAllRecipes.java
@@ -6,6 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ShowAllRecipes extends AppCompatActivity {
 
     @Override
@@ -22,6 +30,34 @@ public class ShowAllRecipes extends AppCompatActivity {
          * Aktivere tilbake knapp i action bar.
          */
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /**
+         * Iterate through recipes database and display all recipes.
+         */
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        String userID = currentUser.getUid();
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference().child("Recipes").child(userID);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String recipeName = ds.child("name").getValue().toString();
+                    String recipeDescription = ds.child("description").getValue().toString();
+
+                    System.out.println(recipeName);
+                    System.out.println(recipeDescription);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     /**
