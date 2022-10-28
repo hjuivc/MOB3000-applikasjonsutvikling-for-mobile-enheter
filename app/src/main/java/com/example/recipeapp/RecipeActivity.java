ea5d87c57df5ce1e401ed4d8c0a8336bba799a5e
Bjørnar Borge <72221999+1d05408acb@users.noreply.github.com>
2022-10-28 10:09:12 +0200

    Update RecipeActivity.java

--- app/src/main/java/com/example/recipeapp/RecipeActivity.java
+++ app/src/main/java/com/example/recipeapp/RecipeActivity.java
@@ -2,12 +2,18 @@ package com.example.recipeapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -16,12 +22,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class RecipeActivity extends AppCompatActivity {
+public class RecipeActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
     private FirebaseUser user;
-    private DatabaseReference reference;
+    private DatabaseReference referenceRecipe;
+    private DatabaseReference referenceIngredients;
 
     private String userID;
+    private Switch favoriteSwitch;
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +39,23 @@ public class RecipeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_recipe);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("Recipes");
+        referenceRecipe = FirebaseDatabase.getInstance().getReference("Recipes");
+        referenceIngredients = FirebaseDatabase.getInstance().getReference("Ingredients");
         userID = user.getUid();
+        favoriteSwitch = findViewById(R.id.txtFavoriteSwitch);
 
         String recipeId = getIntent().getStringExtra("recipeId");
 
         final TextView recipeNameTextView = findViewById(R.id.recipeName);
         final TextView recipeDescriptionTextView = findViewById(R.id.recipeDescription);
         final TextView recipeStepsTextView = findViewById(R.id.recipeStepByStep);
+        final TextView recipeCousineTextView = findViewById(R.id.recipeCousine);
 
-        reference.child(userID).child(recipeId).addListenerForSingleValueEvent(new ValueEventListener() {
+        if (favoriteSwitch != null) {
+            favoriteSwitch.setOnCheckedChangeListener(this);
+        }
+
+        referenceRecipe.child(userID).child(recipeId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 Recipe recipe = snapshot.getValue(Recipe.class);
@@ -47,10 +64,39 @@ public class RecipeActivity extends AppCompatActivity {
                     String recipeName = recipe.name;
                     String recipeDescription = recipe.description;
                     String recipeSteps = recipe.stepbystep;
+                    String recipeCousine = recipe.cuisine;
+                    Integer recipeID = Integer.valueOf(recipe.getRecipeID());
 
                     recipeNameTextView.setText(recipeName);
                     recipeDescriptionTextView.setText(recipeDescription);
                     recipeStepsTextView.setText(recipeSteps);
+                    recipeCousineTextView.setText(recipeCousine);
+
+                    referenceIngredients.child(userID).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for (DataSnapshot ingredientSnapshot : snapshot.getChildren()) {
+                                Ingredients ingredients = ingredientSnapshot.getValue(Ingredients.class);
+
+                                if (ingredients != null) {
+                                    recyclerView = findViewById(R.id.ingredientList);
+                                    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                    DatabaseReference mAuth = FirebaseDatabase.getInstance().getReference().child("Ingredients").child(userId);
+                                    FirebaseRecyclerOptions<Ingredients> options = new FirebaseRecyclerOptions.Builder<Ingredients>()
+                                            .setQuery(mAuth.orderByChild("recipeID").equalTo(recipeID), Ingredients.class)
+                                            .build();
+                                    System.out.println(options);
+                                    IngredientRecAdapter adapter = new IngredientRecAdapter(options);
+                                    recyclerView.setAdapter(adapter);
+                                    adapter.startListening();
+                                    }
+                            }
+                        }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(RecipeActivity.this, "Something wrong happened!", Toast.LENGTH_LONG).show();
+                            }
+                        });
                 }
             }
 
@@ -59,9 +105,6 @@ public class RecipeActivity extends AppCompatActivity {
                 Toast.makeText(RecipeActivity.this, "Noe gikk galt", Toast.LENGTH_LONG).show();
             }
         });
-
-
-
 
         /**
          Legge inn tittel på siden.
@@ -72,6 +115,18 @@ public class RecipeActivity extends AppCompatActivity {
          * Aktivere tilbake knapp i action bar.
          */
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Metode for å aktivere switch vegan i appen.
+     */
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            favoriteSwitch.setText("Favorite");
+        } else {
+            favoriteSwitch.setText("Not favorite");
+        }
     }
 
     /**
@@ -87,5 +142,28 @@ public class RecipeActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnUpdateRecipe:
+                updateRecipe();
+                break;
+            case R.id.btnDeleteRecipe:
+                //TODO: Må legge til kode for å slette recipe.
+                break;
+        }
+    }
+
+    private void updateRecipe() {
+        //TODO: Legge inn kode for å oppdatere oppskrift.
+        boolean vegan = Boolean.parseBoolean(favoriteSwitch.isChecked() ? "true" : "false");
+        System.out.println(vegan);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
