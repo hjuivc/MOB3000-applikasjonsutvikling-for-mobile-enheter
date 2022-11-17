2d8b0c463a18741ed305bfb06e54ab712740e1db
Bjørnar Borge <72221999+borgebjornar@users.noreply.github.com>
2022-11-17 08:23:19 +0100

    Rydder opp i kode
    Basert på anbefalinger fra sonarlint og intellij så har jeg formatert koden litt.

--- app/src/main/java/com/example/recipeapp/User.java
+++ app/src/main/java/com/example/recipeapp/User.java
@@ -1,12 +1,12 @@
 package com.example.recipeapp;
 
 public class User {
-    public String fullName, age, email;
+    public String fullName;
+    public String age;
+    public String email;
 
+    // Default constructor required for calls to DataSnapshot.getValue(User.class)
     public User() {
-        /**
-         * Default constructor required for calls to DataSnapshot.getValue(User.class)
-         */
     }
 
     public User(String fullName, String age, String email) {
