0fb1d139b81091984a760b9a350a070dfa3b7498
Bjørnar Borge <72221999+borgebjornar@users.noreply.github.com>
2022-11-17 08:09:23 +0100

    Ryddet opp i kode på update
    Endret en del for å formatere og gjøre koden penere. Har også fjernet en del ubrukte variabler og kortet ned på kode for å gjøre den mer effektiv.

--- app/src/main/java/com/example/recipeapp/UpdateRecipe.java
+++ app/src/main/java/com/example/recipeapp/UpdateRecipe.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -15,7 +16,6 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
@@ -27,46 +27,42 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Objects;
+
 public class UpdateRecipe extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
-    private FirebaseUser user;
     private DatabaseReference referenceRecipe;
     private DatabaseReference referenceIngredients;
     private String userID;
     private RecyclerView recyclerView;
-    private Button btnConfirmUpdateRecipe;
-    private Switch favoriteSwitch, veganSwitch;
-    private ImageView image;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch favoriteSwitch;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch veganSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_recipe);
 
-
-
-        /**
-         Adding title.
-         */
+        // Adding title.
         this.setTitle(getResources().getString(R.string.activity_update_recipes));
 
-        /**
-         * Activating the "back- button" on the action bar.
-         */
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // Activating the "back- button" on the action bar.
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
-        /**
-         * Adding elements to the cuisine- spinner.
-         */
-        final Spinner recipeCousineSpinner = findViewById(R.id.recipeCousine);
-        ArrayAdapter<CharSequence> adapter_cousine = ArrayAdapter.createFromResource(this, R.array.cousine_array, android.R.layout.simple_spinner_item);
-        adapter_cousine.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        recipeCousineSpinner.setAdapter(adapter_cousine);
+        // Adding elements to the cuisine- spinner.
+        final Spinner recipeCuisineSpinner = findViewById(R.id.recipeCousine);
+        ArrayAdapter<CharSequence> adapter_cuisine = ArrayAdapter.createFromResource(this, R.array.cousine_array, android.R.layout.simple_spinner_item);
+        adapter_cuisine.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        recipeCuisineSpinner.setAdapter(adapter_cuisine);
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         referenceRecipe = FirebaseDatabase.getInstance().getReference("Recipes");
         referenceIngredients = FirebaseDatabase.getInstance().getReference("Ingredients");
-        userID = user.getUid();
+        if (user != null) {
+            userID = user.getUid();
+        }
         favoriteSwitch = findViewById(R.id.favoriteSwitch);
         veganSwitch = findViewById(R.id.veganSwitch);
 
@@ -76,10 +72,10 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
         final EditText recipeDescriptionEditText = findViewById(R.id.recipeDescription);
         final EditText recipeStepsEditText = findViewById(R.id.recipeStepByStep);
 
-        btnConfirmUpdateRecipe = findViewById(R.id.btnUpdateRecipe);
+        Button btnConfirmUpdateRecipe = findViewById(R.id.btnUpdateRecipe);
         btnConfirmUpdateRecipe.setOnClickListener(this);
 
-        image = findViewById(R.id.logo);
+        ImageView image = findViewById(R.id.logo);
         image.setOnClickListener(this);
 
         referenceRecipe.child(userID).child(recipeId).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -91,15 +87,13 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
                     String recipeName = recipe.name;
                     String recipeDescription = recipe.description;
                     String recipeSteps = recipe.stepbystep;
-                    String recipeCousine = recipe.cuisine;
-                    Integer recipeID = Integer.valueOf(recipe.getRecipeID());
-                    String vegan = String.valueOf(recipe.vegan);
-                    String favorite = String.valueOf(recipe.favorite);
+                    String recipeCuisine = recipe.cuisine;
+                    Integer recipeID = recipe.getRecipeID();
 
                     recipeNameEditText.setText(recipeName);
                     recipeDescriptionEditText.setText(recipeDescription);
                     recipeStepsEditText.setText(recipeSteps);
-                    recipeCousineSpinner.setSelection(adapter_cousine.getPosition(recipeCousine));
+                    recipeCuisineSpinner.setSelection(adapter_cuisine.getPosition(recipeCuisine));
 
                     referenceIngredients.child(userID).addValueEventListener(new ValueEventListener() {
                         @Override
@@ -109,12 +103,11 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
 
                                 if (ingredients != null) {
                                     recyclerView = findViewById(R.id.ingredientList);
-                                    String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                    String userId = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
                                     DatabaseReference mAuth = FirebaseDatabase.getInstance().getReference().child("Ingredients").child(userId);
                                     FirebaseRecyclerOptions<Ingredients> options = new FirebaseRecyclerOptions.Builder<Ingredients>()
                                             .setQuery(mAuth.orderByChild("recipeID").equalTo(recipeID), Ingredients.class)
                                             .build();
-                                    System.out.println(options);
                                     IngredientRecAdapter adapter = new IngredientRecAdapter(options);
                                     recyclerView.setAdapter(adapter);
                                     adapter.startListening();
@@ -135,20 +128,15 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
         }
     });
 
-        /**
-         * If favorite is set it will show as set.
-         */
+        // Adding the "onCheckedChanged" listener to the favorite-switch.
         favoriteSwitch.setOnCheckedChangeListener(this);
 
         referenceRecipe.child(userID).child(recipeId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Recipe recipe = dataSnapshot.getValue(Recipe.class);
-                if (recipe.getFavorite() == true) {
-                    favoriteSwitch.setChecked(true);
-                } else {
-                    favoriteSwitch.setChecked(false);
-                }
+                assert recipe != null;
+                favoriteSwitch.setChecked(recipe.getFavorite());
             }
 
             @Override
@@ -157,21 +145,16 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        /**
-         * If vegan is set it will show as set.
-         */
-        final Switch veganSwitch = findViewById(R.id.veganSwitch);
+        // Adding the "onCheckedChanged" listener to the vegan-switch.
+        @SuppressLint("UseSwitchCompatOrMaterialCode") final Switch veganSwitch = findViewById(R.id.veganSwitch);
         veganSwitch.setOnCheckedChangeListener(this);
 
         referenceRecipe.child(userID).child(recipeId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Recipe recipe = dataSnapshot.getValue(Recipe.class);
-                if (recipe.getVegan() == true) {
-                    veganSwitch.setChecked(true);
-                } else {
-                    veganSwitch.setChecked(false);
-                }
+                assert recipe != null;
+                veganSwitch.setChecked(recipe.getVegan());
             }
 
             @Override
@@ -181,41 +164,31 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
         });
     }
 
-    /**
-     * Code for activating the "back- button".
-     */
+    // Adding the back button on the top bar.
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                break;
-            default:
-                break;
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
         }
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnUpdateRecipe:
-                updateRecipe();
-                break;
-            case R.id.logo:
-                startActivity(new Intent(this, ProfileActivity.class));
-                break;
+        int id = v.getId();
+        if (id == R.id.btnUpdateRecipe) {
+            updateRecipe();
+        } else if (id == R.id.logo) {
+            startActivity(new Intent(this, ProfileActivity.class));
         }
     }
 
-    /**
-     * Method for updating the recipe.
-     */
+    // Method for updating the recipe.
     private void updateRecipe() {
         String recipeName = ((EditText) findViewById(R.id.recipeName)).getText().toString();
         String recipeDescription = ((EditText) findViewById(R.id.recipeDescription)).getText().toString();
         String recipeSteps = ((EditText) findViewById(R.id.recipeStepByStep)).getText().toString();
-        String cousine = ((Spinner) findViewById(R.id.recipeCousine)).getSelectedItem().toString();
+        String cuisine = ((Spinner) findViewById(R.id.recipeCousine)).getSelectedItem().toString();
         Boolean vegan = Boolean.parseBoolean(veganSwitch.isChecked() ? "true" : "false");
         Boolean favorite = Boolean.parseBoolean(favoriteSwitch.isChecked() ? "true" : "false");
 
@@ -237,7 +210,7 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
             ((EditText) findViewById(R.id.recipeStepByStep)).requestFocus();
             return;
         }
-        if (cousine.isEmpty()) {
+        if (cuisine.isEmpty()) {
             ((Spinner) findViewById(R.id.recipeCousine)).setPrompt(getResources().getString(R.string.recipe_cuisine));
             ((Spinner) findViewById(R.id.recipeCousine)).requestFocus();
             return;
@@ -249,30 +222,24 @@ public class UpdateRecipe extends AppCompatActivity implements View.OnClickListe
         referenceRecipe.child(userID).child(recipeId).child("name").setValue(recipeName);
         referenceRecipe.child(userID).child(recipeId).child("description").setValue(recipeDescription);
         referenceRecipe.child(userID).child(recipeId).child("stepbystep").setValue(recipeSteps);
-        referenceRecipe.child(userID).child(recipeId).child("cuisine").setValue(cousine);
+        referenceRecipe.child(userID).child(recipeId).child("cuisine").setValue(cuisine);
         // make a toast for the user
         Toast.makeText(UpdateRecipe.this, R.string.toast_recipe_updated, Toast.LENGTH_SHORT).show();
         // back to showAllRecipes
         startActivity(new Intent(UpdateRecipe.this, ShowAllRecipes.class));
 
     }
-    /**
-     * Method for activating the "back- button".
-     */
+    // Method for setting text on the switches using oncheckeschanged
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (veganSwitch.isChecked()) {
             veganSwitch.setText(R.string.vegan);
-
-        } if (favoriteSwitch.isChecked()) {
+        }
+        else if (favoriteSwitch.isChecked()) {
             favoriteSwitch.setText(R.string.update_recipe_favorite);
-
-        } if (!veganSwitch.isChecked()) {
+        } else {
             veganSwitch.setText(R.string.update_recipe_not_vegan);
-        } if (!favoriteSwitch.isChecked()) {
             favoriteSwitch.setText(R.string.update_recipe_not_favorite);
         }
-
     }
-
 }
